350e1e1bd448257a05154dba00778dc86a8f8e79
Jacob Schwartz <jacob@jaschwartz.net>
2021-01-20 15:55:40 -0500

    Break out kappa scaling into an explicit component

--- faroes/elliptical_plasma.py
+++ faroes/elliptical_plasma.py
@@ -64,6 +64,84 @@ class PlasmaBetaNTotal(om.ExplicitComponent):
     def setup_partials(self):
         self.declare_partials('*', '*', method='cs')
 
+class KappaScaling(om.ExplicitComponent):
+    """
+    Inputs
+    ------
+    A : float
+        Aspect ratio
+
+    Outputs
+    -------
+    κ : float
+        Elongation
+
+    κ is determined by a "marginal kappa scaling"
+    from Jon Menard:
+
+    .. math::
+
+       \kappa = 0.95 (1.9 + 1.9 / (A^{1.4})).
+
+    """
+    def initialize(self):
+        self.options.declare('config', default=None)
+
+    def setup(self):
+        if self.options['config'] is not None:
+            self.config = self.options['config']
+            ac = self.config.accessor(['fits'])
+            self.kappa_multiplier = ac(["κ multiplier"])
+            self.κ_ε_scaling_constants = ac(
+                ["marginal κ-ε scaling", "constants"])
+
+        self.add_input("A", desc="Aspect Ratio")
+        self.add_output("κ", lower=0, ref=2, desc="Elongation")
+
+    def marginal_kappa_epsilon_scaling(self, t4_aspect_ratio):
+        """
+        marginal kappa(epsilon) scaling
+
+        Parameters
+        ----------
+        aspect_ratio: float
+
+        References
+        ----------
+        Physics of Plasmas 11, 639 (2004);
+        https://doi.org/10.1063/1.1640623
+        "NSTX, FNSF-ST, DIII-D, EAST"
+        I guess this is how kappa scales with epsilon.
+
+        I'm not sure where it is in the paper, though.
+
+        [T73 from the spreadsheet]
+
+        b12 + c12/(t4^d12)
+
+        Values are from the "Scaling Parameters" sheet
+        """
+        constants = self.κ_ε_scaling_constants
+        sp_b12 = constants[0]
+        sp_c12 = constants[1]
+        sp_d12 = constants[2]
+        return sp_b12 + sp_c12 / (t4_aspect_ratio**sp_d12)
+
+    def compute(self, inputs, outputs):
+        A = inputs["A"]
+        κ = self.kappa_multiplier * self.marginal_kappa_epsilon_scaling(A)
+        outputs["κ"] = κ
+
+    def setup_partials(self):
+        self.declare_partials("κ", "A")
+
+    def compute_partials(self, inputs, J):
+        constants = self.κ_ε_scaling_constants
+        sp_c12 = constants[1]
+        sp_d12 = constants[2]
+        A = inputs["A"]
+        J["κ", "A"] = -self.kappa_multiplier * sp_c12 * sp_d12 A**(-sp_d12 -1)
+
 
 class PlasmaGeometry(om.ExplicitComponent):
     r"""Describes an elliptical plasma.
@@ -128,6 +206,8 @@ class PlasmaGeometry(om.ExplicitComponent):
         m**2, surface area
     V : float
         m**3, volume of the elliptical torus
+    L_pol : float
+        m, Poloidal circumference
     R_min : float
         m, innermost plasma radius at midplane
     R_max : float
@@ -162,6 +242,7 @@ class PlasmaGeometry(om.ExplicitComponent):
                         ref=100,
                         desc="Surface area of the elliptical plasma")
         self.add_output("V", units='m**3', desc="Volume", lower=0, ref=100)
+        self.add_output("L_pol", units="m", desc="Poloidal circumference", lower=0, ref=10)
         self.add_output("R_min",
                         units='m',
                         desc="Inner radius of plasma at midplane")
@@ -190,6 +271,7 @@ class PlasmaGeometry(om.ExplicitComponent):
 
         outputs["R_min"] = R0 - a
         outputs["R_max"] = R0 + a
+        outputs["L_pol"] = util.ellipse_perimeter(a, b)
 
     def marginal_kappa_epsilon_scaling(self, t4_aspect_ratio):
         """
@@ -248,3 +330,4 @@ if __name__ == "__main__":
     prob.set_val('A', 1.6)
 
     prob.run_driver()
+    prob.model.list_outputs()
