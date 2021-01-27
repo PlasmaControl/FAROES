4b1ba0dcc7d9c71e52a0822397c9107254f8e8ce
Jacob Schwartz <jacob@jaschwartz.net>
2021-01-27 17:44:21 -0500

    Add Bootstrap current classes

---
+++ faroes/test/test_bootstrap.py
@@ -0,0 +1,58 @@
+import openmdao.api as om
+from openmdao.utils.assert_utils import assert_check_partials
+from openmdao.utils.assert_utils import assert_near_equal
+
+import faroes.bootstrap
+
+import unittest
+
+
+class TestBootstrapMultiplier(unittest.TestCase):
+    def setUp(self):
+        prob = om.Problem()
+
+        prob.model = faroes.bootstrap.BootstrapMultiplier()
+
+        prob.setup(force_alloc_complex=True)
+
+        prob.set_val("qstar", 1.0)
+        prob.set_val("qmin", 1.0)
+        self.prob = prob
+
+    def test_partials(self):
+        prob = self.prob
+        check = prob.check_partials(out_stream=None, method='cs')
+        assert_check_partials(check)
+
+    def test_value(self):
+        prob = self.prob
+        prob.run_driver()
+        assert_near_equal(prob["bs_mult"], 1.00086, tolerance=1e-4)
+
+
+class TestBootstrapFraction(unittest.TestCase):
+    def setUp(self):
+        prob = om.Problem()
+
+        prob.model = faroes.bootstrap.BootstrapFraction()
+
+        prob.setup(force_alloc_complex=True)
+
+        prob.set_val("bs_mult", 1.0)
+        prob.set_val("βp_th", 1.0)
+        prob.set_val("ε", 0.36)
+        self.prob = prob
+
+    def test_partials(self):
+        prob = self.prob
+        check = prob.check_partials(out_stream=None, method='cs')
+        assert_check_partials(check)
+
+    def test_value(self):
+        prob = self.prob
+        prob.run_driver()
+        assert_near_equal(prob["f_BS"], 0.54, tolerance=1e-4)
+
+
+if __name__ == "__main__":
+    unittest.main()
