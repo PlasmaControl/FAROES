507ae4212efc41051a6caea07773f8c40c07d763
Jacob Schwartz <jacob@jaschwartz.net>
2021-03-03 17:08:45 -0500

    Remove old code from plasma_loop example

--- examples/plasma_loop.py
+++ examples/plasma_loop.py
@@ -166,23 +166,6 @@ class Machine(om.Group):
         self.connect("current.q_star", "bootstrap.q_star")
         self.connect("current.q_min", "bootstrap.q_min")
 
-
-#        self.add_subsystem("radial_build",
-#                           MenardSTRadialBuild(config=config),
-#                           promotes_inputs=['CS R_max'])
-#
-#        self.add_subsystem("magnets",
-#                           MagnetRadialBuild(config=config),
-#                           promotes_inputs=["R0"])
-#        self.connect('plasma.R_max', ['radial_build.plasma R_max'])
-#        self.connect('plasma.R_min', ['radial_build.plasma R_min'])
-#
-#        self.connect('radial_build.Ob TF R_min', ['magnets.r_iu'])
-#        self.connect('radial_build.Ib TF R_min', ['magnets.r_is'])
-#        self.connect('radial_build.Ib TF R_max', ['magnets.r_ot'])
-#
-#        self.connect('magnets.Ob TF R_out', ['radial_build.Ob TF R_out'])
-
 if __name__ == "__main__":
     prob = om.Problem()
 
@@ -190,31 +173,6 @@ if __name__ == "__main__":
     prob.model = Machine(config=uc)
 
     model = prob.model
-
-    # prob.driver = om.ScipyOptimizeDriver()
-    # prob.driver.options['optimizer'] = 'SLSQP'
-    # prob.driver.options['disp'] = True
-
-    # prob.model.add_design_var('plasma.A', lower=1.6, upper=4.0, ref=2.0)
-    # prob.model.add_design_var('CS R_max',
-    #                              lower=0.02,
-    #                              upper=1.0,
-    #                              ref=0.3,
-    #                              units='m')
-    # prob.model.add_design_var('magnets.r_im', lower=0.05, upper=1.0, ref=0.3)
-    # prob.model.add_design_var('magnets.j_HTS', lower=10, upper=300, ref=100)
-
-    # prob.model.add_objective('magnets.obj')
-
-    # set constraints
-    # prob.model.add_constraint('magnets.constraint_max_stress', lower=0)
-    # prob.model.add_constraint('magnets.constraint_B_on_coil', lower=0)
-    # prob.model.add_constraint('magnets.constraint_wp_current_density',
-    #                            lower=0)
-    # prob.model.add_constraint('magnets.r_im_is_constraint', lower=0)
-    # prob.model.add_constraint('magnets.A_s', lower=0)
-    # prob.model.add_constraint('magnets.A_m', lower=0)
-    # prob.model.add_constraint('magnets.A_t', lower=0)
 
     prob.setup()
     # prob.check_config(checks=['unconnected_inputs'])
@@ -234,7 +192,6 @@ if __name__ == "__main__":
     prob.set_val("<n_e>", 1.26, units="n20")
 
     prob.model.nonlinear_solver = om.NewtonSolver(solve_subsystems=True)
-    # prob.model.nonlinear_solver = om.NonlinearBlockGS()
     prob.model.nonlinear_solver.options['iprint'] = 2
     prob.model.nonlinear_solver.options['maxiter'] = 20
     prob.model.linear_solver = om.DirectSolver()
@@ -243,13 +200,6 @@ if __name__ == "__main__":
     prob.set_val("radiation.rad.P_loss", 91, units="MW")
 
     prob.run_driver()
-
-    # prob.check_totals(of=['NBIslowing.Wfast'], wrt=['confinementtime.Ip'])
-
-    #    prob.set_val('magnets.n_coil', 18)
-    #    prob.set_val('magnets.windingpack.j_eff_max', 160)
-    #    prob.set_val('magnets.windingpack.f_HTS', 0.76)
-    #    prob.set_val("magnets.magnetstructure_props.Young's modulus", 220)
 
     all_inputs = prob.model.list_inputs(values=True,
                                         print_arrays=True,
