904d4989f85fad5c4b70ff0c51651a4e9618d965
Jacob Schwartz <jacob@jaschwartz.net>
2021-03-05 16:23:12 -0500

    Put more menard_model geometry in TotalRadialBuild

--- examples/menard_model.py
+++ examples/menard_model.py
@@ -50,6 +50,41 @@ class TotalRadialBuild(om.Group):
 
         self.connect('magnets.Ob TF R_out', ['radial_build.Ob TF R_out'])
 
+        self.add_subsystem(
+            "blanket_sh",
+            MenardSTBlanketAndShieldMagnetProtection(config=config))
+        self.connect("radial_build.props.Ib blanket thickness",
+                     "blanket_sh.Ib blanket thickness")
+        self.connect("radial_build.props.Ib WC shield thickness",
+                     "blanket_sh.Ib WC shield thickness")
+        self.connect("radial_build.props.Ib WC VV shield thickness",
+                     "blanket_sh.Ib WC VV shield thickness")
+
+        # this just needs to be run after the radial build
+        self.add_subsystem("blanketgeom", MenardSTBlanketAndShieldGeometry(),
+                promotes_inputs=["κ"])
+        self.connect("radial_build.props.Ob SOL width",
+                     "blanketgeom.Ob SOL width")
+        self.connect("radial_build.ib.blanket R_max",
+                     "blanketgeom.Ib blanket R_out")
+        self.connect("radial_build.ib.blanket R_min",
+                     "blanketgeom.Ib blanket R_in")
+
+        self.connect("radial_build.ob.blanket R_in",
+                     "blanketgeom.Ob blanket R_in")
+        self.connect("radial_build.ob.blanket R_out",
+                     "blanketgeom.Ob blanket R_out")
+
+        self.connect("radial_build.ib.WC shield R_max",
+                     "blanketgeom.Ib WC shield R_out")
+        self.connect("radial_build.ib.WC shield R_min",
+                     "blanketgeom.Ib WC shield R_in")
+        self.connect("radial_build.ib.WC VV shield R_max",
+                     "blanketgeom.Ib WC VV shield R_out")
+        self.connect("radial_build.ib.WC VV shield R_min",
+                     "blanketgeom.Ib WC VV shield R_in")
+
+
 
 class Machine(om.Group):
     def initialize(self):
@@ -61,15 +96,16 @@ class Machine(om.Group):
         self.add_subsystem("plasmageom",
                            MenardPlasmaGeometry(config=config),
                            promotes_inputs=["R0", ("A", "aspect_ratio")],
-                           promotes_outputs=["ε", "κa", "V"])
+                           promotes_outputs=["ε", "κ", "κa", "V"])
 
         self.add_subsystem("totalbuild",
                            TotalRadialBuild(config=config),
-                           promotes_inputs=["R0"])
+                           promotes_inputs=["R0", "κ"])
 
         self.connect('plasmageom.R_max', ['totalbuild.plasma R_max'])
         self.connect('plasmageom.R_min', ['totalbuild.plasma R_min'])
 
+        self.connect("plasmageom.a", "totalbuild.blanketgeom.a")
 
         mpl = MenardPlasmaLoop(config=config)
         self.add_subsystem(
@@ -83,18 +119,7 @@ class Machine(om.Group):
 
         self.connect("totalbuild.Bt", ["plasma.Bt", "SOL.Bt"])
 
-        # this needs to be run after the radial build but before
-        # the magnet cryo calculations
-        self.add_subsystem(
-            "blanket_sh",
-            MenardSTBlanketAndShieldMagnetProtection(config=config))
-        self.connect("totalbuild.radial_build.props.Ib blanket thickness",
-                     "blanket_sh.Ib blanket thickness")
-        self.connect("totalbuild.radial_build.props.Ib WC shield thickness",
-                     "blanket_sh.Ib WC shield thickness")
-        self.connect("totalbuild.radial_build.props.Ib WC VV shield thickness",
-                     "blanket_sh.Ib WC VV shield thickness")
-        self.connect("blanket_sh.Eff Sh+Bl n thickness", "magcryo.Δr_sh")
+        self.connect("totalbuild.blanket_sh.Eff Sh+Bl n thickness", "magcryo.Δr_sh")
 
         # magnet heating model;
         # total power in blanket
@@ -113,9 +138,8 @@ class Machine(om.Group):
         # SOL and divertor model. Useful for constraints.
         self.add_subsystem("SOL",
                            SOLAndDivertor(config=config),
-                           promotes_inputs=["R0"])
+                           promotes_inputs=["R0", "κ"])
         self.connect("plasmageom.a", "SOL.a")
-        self.connect("plasmageom.κ", "SOL.κ")
         self.connect("plasma.P_heat.P_heat", "SOL.P_heat")
         self.connect("plasma.Ip", "SOL.Ip")
         self.connect("plasma.current.q_star", "SOL.q_star")
@@ -131,35 +155,10 @@ class Machine(om.Group):
         self.connect("plasma.DTfusion.rate_fus", ["q_n_IB.S"])
         self.connect("q_n_IB.q_n", ["q_n.q_n_IB"])
 
-        # this just needs to be run after the radial build
-        self.add_subsystem("blanketgeom", MenardSTBlanketAndShieldGeometry())
-        self.connect("plasmageom.a", "blanketgeom.a")
-        self.connect("plasmageom.κ", "blanketgeom.κ")
-        self.connect("totalbuild.radial_build.props.Ob SOL width",
-                     "blanketgeom.Ob SOL width")
-        self.connect("totalbuild.radial_build.ib.blanket R_max",
-                     "blanketgeom.Ib blanket R_out")
-        self.connect("totalbuild.radial_build.ib.blanket R_min",
-                     "blanketgeom.Ib blanket R_in")
-
-        self.connect("totalbuild.radial_build.ob.blanket R_in",
-                     "blanketgeom.Ob blanket R_in")
-        self.connect("totalbuild.radial_build.ob.blanket R_out",
-                     "blanketgeom.Ob blanket R_out")
-
-        self.connect("totalbuild.radial_build.ib.WC shield R_max",
-                     "blanketgeom.Ib WC shield R_out")
-        self.connect("totalbuild.radial_build.ib.WC shield R_min",
-                     "blanketgeom.Ib WC shield R_in")
-        self.connect("totalbuild.radial_build.ib.WC VV shield R_max",
-                     "blanketgeom.Ib WC VV shield R_out")
-        self.connect("totalbuild.radial_build.ib.WC VV shield R_min",
-                     "blanketgeom.Ib WC VV shield R_in")
-
 
         self.add_subsystem("maglife", MenardMagnetLifetime(config=config))
         self.connect("q_n_IB.q_n", "maglife.q_n_IB")
-        self.connect("blanket_sh.Shielding factor", "maglife.Shielding factor")
+        self.connect("totalbuild.blanket_sh.Shielding factor", "maglife.Shielding factor")
 
 
 if __name__ == "__main__":
@@ -172,7 +171,7 @@ if __name__ == "__main__":
 
     ivc = om.IndepVarComp()
     ivc.add_output("R0", 3.0, units="m")
-    ivc.add_output("aspect_ratio", 2.0)
+    ivc.add_output("aspect_ratio", 1.60)
     prob.model.add_subsystem("P", ivc, promotes_outputs=["*"])
 
     prob.driver = om.ScipyOptimizeDriver()
@@ -187,14 +186,14 @@ if __name__ == "__main__":
                               upper=0.05,
                               ref=0.03,
                               units='m')
-    prob.model.add_design_var('aspect_ratio', lower=1.6, upper=2.20, ref=1.6)
+    prob.model.add_design_var('aspect_ratio', lower=1.6, upper=1.60, ref=1.6)
     prob.model.add_design_var('totalbuild.magnets.f_im',
                               lower=0.50,
                               upper=0.95,
                               ref=0.5)
     prob.model.add_design_var('totalbuild.magnets.j_HTS',
                               lower=50,
-                              upper=100,
+                              upper=160,
                               ref=100,
                               units="MA/m**2")
 
@@ -209,18 +208,18 @@ if __name__ == "__main__":
                               lower=0)
     prob.model.add_constraint(
         'totalbuild.magnets.constraint_wp_current_density', lower=0)
-    prob.model.add_constraint('totalbuild.magnets.A_s', lower=0)
-    prob.model.add_constraint('totalbuild.magnets.A_m', lower=0)
-    prob.model.add_constraint('totalbuild.magnets.A_t', lower=0)
+    # prob.model.add_constraint('totalbuild.magnets.A_s', lower=0)
+    # prob.model.add_constraint('totalbuild.magnets.A_m', lower=0)
+    # prob.model.add_constraint('totalbuild.magnets.A_t', lower=0)
 
     prob.setup()
-    prob.check_config(checks=['unconnected_inputs'])
+    # prob.check_config(checks=['unconnected_inputs'])
 
     prob.set_val('totalbuild.magnets.n_coil', 18)
 
     # initial values for design variables
-    prob.set_val("totalbuild.magnets.f_im", 0.60)
-    prob.set_val("totalbuild.magnets.j_HTS", 100, units="MA/m**2")
+    prob.set_val("totalbuild.magnets.f_im", 0.30)
+    prob.set_val("totalbuild.magnets.j_HTS", 130, units="MA/m**2")
     prob.set_val("totalbuild.CS R_max", 0.03, units="m")
 
     build = prob.model.totalbuild
@@ -230,10 +229,10 @@ if __name__ == "__main__":
     build.linear_solver = om.DirectSolver()
 
     # initial inputs for intermediate variables
-    prob.set_val("plasma.Hbalance.H", 1.5)
-    prob.set_val("plasma.Ip", 17., units="MA")
-    prob.set_val("plasma.<n_e>", 1.20, units="n20")
-    prob.set_val("plasma.radiation.rad.P_loss", 100, units="MW")
+    prob.set_val("plasma.Hbalance.H", 1.70)
+    prob.set_val("plasma.Ip", 14., units="MA")
+    prob.set_val("plasma.<n_e>", 1.32, units="n20")
+    prob.set_val("plasma.radiation.rad.P_loss", 150, units="MW")
 
     mpl = prob.model.plasma
     newton = mpl.nonlinear_solver = om.NewtonSolver(solve_subsystems=True)
@@ -256,6 +255,6 @@ if __name__ == "__main__":
     all_inputs = prob.model.list_inputs(values=True,
                                         print_arrays=True,
                                         units=True)
-    # all_outputs = prob.model.list_outputs(values=True,
-    #                                       print_arrays=True,
-    #                                       units=True)
+    all_outputs = prob.model.list_outputs(values=True,
+                                          print_arrays=True,
+                                          units=True)
