7ebe7df53e83d51d47ab9797fc5bf5385f78d780
Jacob Schwartz <jacob@jaschwartz.net>
2021-04-08 16:41:43 -0400

    Add ThreeEllipseArcDee coil as a generalization
    This allows quarter-elliptical arcs for the inboard half of the magnet.

--- faroes/test/test_threearcdeecoil.py
+++ faroes/test/test_threearcdeecoil.py
@@ -46,6 +46,26 @@ class TestThreeArcDeeTFSetAdaptorZsmall(unittest.TestCase):
         assert_check_partials(check)
 
 
+class TestThreeEllipseArcDeeTFSetAdaptor(unittest.TestCase):
+    def setUp(self):
+        prob = om.Problem()
+        prob.model = coil.ThreeEllipseArcDeeTFSetAdaptor()
+        prob.setup(force_alloc_complex=True)
+
+        prob.set_val("Ib TF R_out", 2.0, units="m")
+        prob.set_val("Ob TF R_in", 6.0, units="m")
+        prob.set_val("Z_min", 5.0, units="m")
+        prob.set_val("f_c", 0.5)
+        prob.set_val("f_hhs", 0.5)
+        prob.set_val("Z_1", 0.5)
+        self.prob = prob
+
+    def test_partials(self):
+        prob = self.prob
+        check = prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(check)
+
+
 class TestThreeArcDeeTFSet(unittest.TestCase):
     def setUp(self):
         prob = om.Problem()
@@ -65,6 +85,41 @@ class TestThreeArcDeeTFSet(unittest.TestCase):
         prob.set_val("R0", 3)
         prob.set_val("Ib TF R_out", 0.94)
         prob.set_val("r_c", 1.2)
+        prob.set_val("e_a", 3)
+        prob.set_val("hhs", 3)
+        prob.set_val("θ", θ)
+
+        check = prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(check)
+
+        self.prob = prob
+
+    def test_partials(self):
+        prob = self.prob
+        check = prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(check)
+
+
+class TestThreeEllipseArcDeeTFSet(unittest.TestCase):
+    def setUp(self):
+        prob = om.Problem()
+
+        θ = np.linspace(-pi, pi, 31, endpoint=True)
+
+        prob.model.add_subsystem("ivc",
+                                 om.IndepVarComp("θ", val=θ),
+                                 promotes_outputs=["*"])
+
+        prob.model.add_subsystem("tadTF",
+                                 coil.ThreeEllipseArcDeeTFSet(),
+                                 promotes_inputs=["*"])
+
+        prob.setup(force_alloc_complex=True)
+
+        prob.set_val("R0", 3)
+        prob.set_val("Ib TF R_out", 0.94)
+        prob.set_val("e1_a", 1.0)
+        prob.set_val("e1_b", 1.5)
         prob.set_val("e_a", 3)
         prob.set_val("hhs", 3)
         prob.set_val("θ", θ)
--- faroes/threearcdeecoil.py
+++ faroes/threearcdeecoil.py
@@ -6,6 +6,116 @@ from scipy.constants import pi
 import numpy as np
 
 
+class ThreeEllipseArcDeeTFSetAdaptor(om.ExplicitComponent):
+    r"""Helps generate feasible ThreeEllipseArcDee solutions
+
+    Ensures that only ThreeEllipseArcDee shapes with peaks at or above
+    a height Z_min are generated.
+
+    Notes
+    -----
+    This does not mean that all magnets generated will fit around a given
+    plasma. It only removes a portion of the non-feasible space.
+
+    Inputs
+    ------
+    Ib TF R_out: float
+        m, Inboard TF outer radius
+    Ob TF R_in: float
+        m, Outboard TF inner radius
+    Z_min : float
+        m, minimum height for inner edge of magnet's crown
+
+    f_c : float
+        Fraction of possible bore taken up by the inboard arc
+          Good as a design variable, 0.01 < f_c < 0.99
+    f_hhs : float
+        Fraction of height taken up by the straight segment
+          Good as a design variable, 0.01 < f_hhs < 0.99
+    Z_1 : float
+        m, Extra height above Z_min for the inner edge of the
+          magnet crown. Good as a design variable, 0 < Z_1 < 5m.
+
+    Outputs
+    -------
+    hhs : float
+        m, half-height of the straight segment
+    e_a : float
+        m, elliptical arc horizontal semi-major axis
+    e1_a : float
+        m, inboard arc horizontal radius
+    e1_b : float
+        m, inboard arc vertical radius
+    """
+    def setup(self):
+        self.add_input("Ib TF R_out", units="m")
+        self.add_input("Ob TF R_in", units="m")
+        self.add_input("Z_min", units="m")
+
+        self.add_input("f_c", val=0.5)
+        self.add_input("f_hhs", val=0.5)
+        self.add_input("Z_1", units="m", val=0.5)
+
+        self.add_output("hhs", units="m", lower=0)
+        self.add_output("e1_a", units="m", lower=0)
+        self.add_output("e1_b", units="m", lower=0)
+        self.add_output("e_a", units="m", lower=0)
+
+    def compute(self, inputs, outputs):
+        f_c = inputs["f_c"]
+        f_hhs = inputs["f_hhs"]
+
+        z_min = inputs["Z_min"]
+        z_1 = inputs["Z_1"]
+
+        span = inputs["Ob TF R_in"] - inputs["Ib TF R_out"]
+        e1_a = f_c * span
+        e_a = span - e1_a
+
+        h = z_min + z_1
+        hhs = h * f_hhs
+        e1_b = h - hhs
+
+        outputs["e1_a"] = e1_a
+        outputs["e1_b"] = e1_b
+        outputs["e_a"] = e_a
+        outputs["hhs"] = hhs
+
+    def setup_partials(self):
+        self.declare_partials(["e_a", "e1_a"],
+                              ["Ob TF R_in", "Ib TF R_out", "f_c"])
+        self.declare_partials(["e1_b", "hhs"], ["Z_1", "Z_min", "f_hhs"])
+
+    def compute_partials(self, inputs, J):
+        r_in = inputs["Ib TF R_out"]
+        r_out = inputs["Ob TF R_in"]
+        span = r_out - r_in
+        f_c = inputs["f_c"]
+        f_hhs = inputs["f_hhs"]
+        z_1 = inputs["Z_1"]
+        z_min = inputs["Z_min"]
+
+        span = inputs["Ob TF R_in"] - inputs["Ib TF R_out"]
+        h = z_min + z_1
+
+        dspan_drob = 1
+        dspan_drib = -1
+        J["hhs", "Z_1"] = f_hhs
+        J["hhs", "Z_min"] = f_hhs
+        J["hhs", "f_hhs"] = h
+
+        J["e1_b", "Z_1"] = 1 - f_hhs
+        J["e1_b", "Z_min"] = 1 - f_hhs
+        J["e1_b", "f_hhs"] = -h
+
+        J["e1_a", "Ob TF R_in"] = dspan_drob * f_c
+        J["e1_a", "Ib TF R_out"] = dspan_drib * f_c
+        J["e1_a", "f_c"] = span
+        J["e_a", "Ob TF R_in"] = dspan_drob * (1 - f_c)
+        J["e_a", "Ib TF R_out"] = dspan_drib * (1 - f_c)
+        J["e_a", "f_c"] = -span
+
+
 class ThreeArcDeeTFSetAdaptor(om.ExplicitComponent):
     r"""Helps generate feasible ThreeArcDee solutions
 
@@ -120,6 +230,255 @@ class ThreeArcDeeTFSetAdaptor(om.ExplicitComponent):
         J["hhs", "Ib TF R_out"] = -J["r_c", "Ib TF R_out"]
         J["hhs", "Z_1"] = 1 - J["r_c", "Z_1"]
         J["hhs", "Z_min"] = 1 - J["r_c", "Z_min"]
+
+
+class ThreeEllipseArcDeeTFSet(om.ExplicitComponent):
+    r"""Three-arc Dee magnet set with all ellipses
+
+    This magnet has a profile composed of a vertical line
+    (the inner leg), two quarter-ellipse arcs, and an outer
+    half-ellipse arc.
+
+    Inputs
+    ------
+    Ib TF R_out : float
+        m, inboard leg outer radius
+    hhs : float
+        m, half-height of the straight segment
+    e_a : float
+        m, elliptical arc horizontal semi-major axis
+    e1_a : float
+        m, inboard elliptical arc horizontal semi-major axis
+    e1_b : float
+        m, inboard elliptical arc horizontal semi-major axis
+    R0 : float
+        m, plasma major radius
+    θ   : array
+        Angles relative to the magnetic axis at which to
+           evaluate the distance to the magnet.
+
+    Outputs
+    -------
+    e_b : float
+        m, elliptical arc vertical semi-major axis
+    d_sq : float
+        m**2, Squared distance to points on the inner perimeter,
+           at angle θ from the magnetic axis
+    arc length: float
+        m, inner perimeter of the magnet
+    V_enc : float
+        m**3, magnetized volume enclosed by the set
+    half-height : float
+        m, half the vertical height of the conductors
+    bore : float
+        m, Horizontal span of the interior bore
+    e_κ : float
+        "Elongation" of the outer elliptical arc
+    """
+    def setup(self):
+        self.add_input("R0", units="m", desc="Plasma major radius")
+        self.add_input("Ib TF R_out",
+                       units="m",
+                       desc="Inboard TF leg outer radius")
+        self.add_input("e_a",
+                       units="m",
+                       desc="Elliptical arc horizontal semi-major axis")
+        self.add_input("hhs",
+                       units="m",
+                       desc="Half-height of the straight segment")
+        self.add_input("e1_a", units="m")
+        self.add_input("e1_b", units="m")
+        self.add_input("θ", shape_by_conn=True)
+
+        self.add_output("e_b",
+                        units="m",
+                        desc="Elliptical arc vertical semi-major axis")
+        self.add_output("arc length",
+                        units="m",
+                        desc="Inner perimeter of the magnet")
+        V_enc_ref = 1e3
+        self.add_output("Ob TF R_in", units="m", ref=10, lower=0)
+        self.add_output("constraint_axis_within_coils", units="m", ref=1)
+        self.add_output("V_enc",
+                        units="m**3",
+                        lower=0,
+                        ref=V_enc_ref,
+                        desc="magnetized volume enclosed by the set")
+        self.add_output("d_sq", units="m**2", copy_shape="θ")
+        self.add_output("half-height",
+                        units="m",
+                        lower=0,
+                        desc="Average semi-major axis of the magnet")
+        self.add_output("bore",
+                        units="m",
+                        lower=0,
+                        desc="Horizontal span of the interior bore")
+        self.add_output("e_κ",
+                        lower=0,
+                        desc="Elongation of the elliptical arc")
+
+    def compute(self, inputs, outputs):
+        size = self._get_var_meta("θ", "size")
+        R0 = inputs["R0"]
+        r_ot = inputs["Ib TF R_out"]
+
+        e_a = inputs["e_a"]
+        e1_a = inputs["e1_a"]
+        e1_b = inputs["e1_b"]
+        hhs = inputs["hhs"]
+        θ_all = inputs["θ"]
+        e_b = hhs + e1_b
+        outputs["e_b"] = e_b
+        outputs["e_κ"] = e_b / e_a
+        outputs["Ob TF R_in"] = r_ot + e1_a + e_a
+        outputs["constraint_axis_within_coils"] = r_ot + e1_a + e_a - R0
+
+        arc_length = (util.ellipse_perimeter_ramanujan(e_a, e_b) / 2 +
+                      util.ellipse_perimeter_ramanujan(e1_a, e1_b) / 2 +
+                      2 * hhs)
+        outputs["arc length"] = arc_length
+
+        # radius of the transition between the inboard quarter-ellipse and
+        # outboard half-ellipse
+        R = r_ot + e1_a
+        v_1 = util.half_ellipse_torus_volume(R, e_a, e_b)
+        v_2 = util.half_ellipse_torus_volume(R, -e1_a, e1_b)
+        v_3 = pi * 2 * hhs * (R**2 - (R - e1_a)**2)
+        outputs["V_enc"] = v_1 + v_2 + v_3
+        outputs["bore"] = e1_a + e_a
+
+        θ1 = cs_safe_arctan2(e_b, R - R0)
+        θ2 = cs_safe_arctan2(hhs, r_ot - R0)
+        θ3 = cs_safe_arctan2(-hhs, r_ot - R0)
+        θ4 = cs_safe_arctan2(-e_b, R - R0)
+
+        θ = θ_all
+        on_ellipse = (θ4 < θ) * (θ < θ1)
+        on_upper_arc = (θ1 <= θ) * (θ < θ2)
+        on_lower_arc = (θ3 < θ) * (θ <= θ4)
+        on_straight = np.logical_or(θ2 <= θ, θ <= θ3)
+        # abbreviations for easier writing
+
+        d2 = np.zeros(size, dtype=np.cdouble)
+
+        θ = θ_all[on_straight]
+        d2[on_straight] = (R0 - r_ot)**2 / np.cos(θ)**2
+
+        θ = θ_all[on_lower_arc]
+        d = util.polar_offset_ellipse(a=e1_a, b=e1_b, x=R - R0, y=-hhs, t=θ)
+        d2[on_lower_arc] = d**2
+
+        θ = θ_all[on_upper_arc]
+        d = util.polar_offset_ellipse(a=e1_a, b=e1_b, x=R - R0, y=+hhs, t=θ)
+        d2[on_upper_arc] = d**2
+
+        # distance to ellipse
+        θ = θ_all[on_ellipse]
+        d = util.polar_offset_ellipse(a=e_a, b=e_b, x=R - R0, y=0, t=θ)
+        d2[on_ellipse] = d**2
+
+        outputs["d_sq"] = d2
+
+        outputs["half-height"] = e_b
+
+    def setup_partials(self):
+        self.declare_partials("e_b", ["hhs", "e1_b"], val=1)
+        self.declare_partials("half-height", ["hhs", "e1_b"], val=1)
+        self.declare_partials("arc length", ["e_a", "hhs", "e1_a", "e1_b"])
+        self.declare_partials("V_enc",
+                              ["Ib TF R_out", "e_a", "hhs", "e1_a", "e1_b"])
+        self.declare_partials("d_sq",
+                              ["Ib TF R_out", "e_a", "hhs", "e1_a", "e1_b"],
+                              method="cs")
+        self.declare_partials("d_sq", ["R0"], method="cs")
+        self.declare_partials("d_sq", ["θ"], method="cs")
+        self.declare_partials("Ob TF R_in", ["e_a", "e1_a", "Ib TF R_out"],
+                              val=1)
+        self.declare_partials("constraint_axis_within_coils",
+                              ["e_a", "e1_a", "Ib TF R_out"],
+                              val=1)
+        self.declare_partials("constraint_axis_within_coils", ["R0"], val=-1)
+        self.declare_partials("bore", ["e1_a", "e_a"], val=1)
+        self.declare_partials("e_κ", ["e1_b", "e_a", "hhs"])
+
+    def compute_partials(self, inputs, J):
+        r_ot = inputs["Ib TF R_out"]
+
+        e_a = inputs["e_a"]
+        hhs = inputs["hhs"]
+        e1_a = inputs["e1_a"]
+        e1_b = inputs["e1_b"]
+        e_b = hhs + e1_b
+
+        deb_dhhs = 1
+        deb_de1b = 1
+
+        # outboard half-ellipse
+        dfullarclen = util.ellipse_perimeter_ramanujan_derivatives(e_a, e_b)
+        darclen_dea = dfullarclen["a"] / 2
+        darclen_deb = dfullarclen["b"] / 2
+
+        # two inboard quarter-ellipses
+        dibfullarclen = util.ellipse_perimeter_ramanujan_derivatives(
+            e1_a, e1_b)
+        dibarclen_dea = dibfullarclen["a"] / 2
+        dibarclen_deb = dibfullarclen["b"] / 2
+        J["arc length", "e_a"] = darclen_dea
+        J["arc length", "hhs"] = darclen_deb * deb_dhhs + 2
+        J["arc length", "e1_a"] = dibarclen_dea
+        J["arc length", "e1_b"] = dibarclen_deb + darclen_deb * deb_de1b
+
+        R = r_ot + e1_a
+        dR_drot = 1
+        dR_de1a = 1
+        dv1 = util.half_ellipse_torus_volume_derivatives(R, e_a, e_b)
+        dv1_dR, dv1_dea, dv1_deb = dv1["R"], dv1["a"], dv1["b"]
+        dv2 = util.half_ellipse_torus_volume_derivatives(R, -e1_a, e1_b)
+        dv2_dR, dv2_de1a, dv2_de1b = dv2["R"], dv2["a"], dv2["b"]
+
+        dv3_dhhs = 2 * pi * (R**2 - (R - e1_a)**2)
+        dv3_dR = 4 * pi * hhs * e1_a
+        dv3_de1a = 4 * pi * hhs * (R - e1_a)
+
+        J["V_enc", "e_a"] = dv1_dea
+        J["V_enc", "hhs"] = dv1_deb * deb_dhhs + dv3_dhhs
+        J["V_enc", "e1_a"] = (dv1_dR * dR_de1a - dv2_de1a + dv2_dR * dR_de1a +
+                              dv3_de1a + dv3_dR * dR_de1a)
+        J["V_enc", "e1_b"] = (dv1_deb * deb_de1b + dv2_de1b)
+        J["V_enc", "Ib TF R_out"] = (dv1_dR * dR_drot + dv2_dR * dR_drot +
+                                     dv3_dR * dR_drot)
+        J["e_κ", "hhs"] = 1 / e_a
+        J["e_κ", "e1_b"] = 1 / e_a
+        J["e_κ", "e_a"] = -(hhs + e1_b) / e_a**2
+
+    def plot(self, ax=None, **kwargs):
+        size = 100
+        color = "black"
+
+        if "color" in kwargs.keys():
+            color = kwargs[color]
+
+        t = np.linspace(0, 1, 100)
+        r_ot = self.get_val("Ib TF R_out")
+        straight_R = r_ot * np.ones(size)
+        hhs = self.get_val("hhs")
+        straight_Z = t * (2 * hhs) - hhs
+        ax.plot(straight_R, straight_Z, color=color, **kwargs)
+
+        # quarter ellipses
+        e1_a = self.get_val("e1_a")
+        e1_b = self.get_val("e1_b")
+        c_R = r_ot + e1_a * (1 - np.cos(t * np.pi / 2))
+        c_Z = hhs + e1_b * (np.sin(t * np.pi / 2))
+        ax.plot(c_R, c_Z, color=color, **kwargs)
+        ax.plot(c_R, -c_Z, color=color, **kwargs)
+
+        # outer half-ellipse
+        e_a = self.get_val("e_a")
+        e_b = self.get_val("e_b")
+        el_R = r_ot + e1_a + e_a * np.cos(t * np.pi - np.pi / 2)
+        el_Z = e_b * np.sin(t * np.pi - np.pi / 2)
+        ax.plot(el_R, el_Z, color=color, **kwargs)
 
 
 class ThreeArcDeeTFSet(om.ExplicitComponent):
