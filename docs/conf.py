01b9f1f716a5504efeea9338c2ffa6109d867fe0
Jacob Schwartz <jacob@jaschwartz.net>
2021-08-04 17:56:28 -0400

    Switch to readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'FAROES'
 author = 'Jacob Schwartz'
-copyright = f'2020, {author}'
-# uncomment this in 2021
-# copyright = f'2020-{datetime.utcnow().year}, {author}'
+copyright = f'2020-{datetime.utcnow().year}, {author}'
 
 # -- General configuration ---------------------------------------------------
 
@@ -35,6 +33,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
 ]
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
@@ -64,7 +63,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
