28dfc6a0ce6ca030ddea082b3017c2a6cc74ba69
Jacob Schwartz <jacob@jaschwartz.net>
2022-05-12 20:54:29 -0400

    Add fusrate 'subpackage'
    Note that this is not an actual git submodule

---
+++ faroes/fusrate/endf.py
@@ -0,0 +1,61 @@
+from faroes.fusrate.load_data import load_data_file
+from scipy.interpolate import InterpolatedUnivariateSpline
+import numpy as np
+
+
+class LogLogExtrapolation():
+    r"""Interpolate and extrapolate in log-log space
+
+    with a straight-line end in log-log space
+    """
+    SMALL = 1e-50  # to prevent errors with log of 0
+
+    def __init__(self, x, y, linear_extension=True):
+        r"""
+        x: array_like
+        y: array_like
+        """
+        self.x = x
+        self.y = y
+        self.max_x = max(x)
+        self.min_x = min(x)
+        logx = np.log(x + self.SMALL)
+        logy = np.log(y)
+        data = np.array([logx, logy]).T
+
+        # linear extension in loglog space
+        if linear_extension:
+            last_two = data[-2:]
+            last = last_two[-1]
+            Δ = (last_two[-1] - last_two[-2])
+            subsequent_points = last + np.outer([1, 2, 3], Δ)
+            self.data = np.append(data, subsequent_points, axis=0)
+        else:
+            self.data = data
+
+        self.logx, self.logy = self.data.T
+
+        # self.interpolator = interp1d(self.logx, self.logy, kind='quadratic',
+        #         fill_value='extrapolate', assume_sorted=True)
+        self.interpolator = InterpolatedUnivariateSpline(self.logx,
+                                                         self.logy,
+                                                         k=2,
+                                                         ext=0)
+
+    def __call__(self, newx):
+        r"""Generate new values
+        newx: array_like
+        """
+        log_new = np.log(newx + self.SMALL)
+        log_newy = self.interpolator(log_new)
+        return np.exp(log_newy)
+
+
+if __name__ == '__main__':
+    data_name = 'cross_section_t(d,n)a.csv'
+    x, y = load_data_file(data_name)
+    lle = LogLogExtrapolation(x, y, linear_extension=True)
+
+    newx = np.logspace(4, 12, 80)
+
+    print(lle(newx))
---
+++ faroes/fusrate/load_data.py
@@ -0,0 +1,42 @@
+from faroes.fusrate.reactionnames import reaction_name_simplify
+from faroes.fusrate.reactionnames import reaction_name_to_endf
+
+from importlib import resources
+
+import numpy as np
+
+DEFAULT_DATA_DIR = 'faroes.fusrate.data'
+CROSS_SECTION_PREFIX = 'cross_section_'
+CROSS_SECTION_FILETYPE = '.csv'
+
+
+def locate_data_file(dname):
+    if resources.is_resource(DEFAULT_DATA_DIR, dname):
+        with resources.path(DEFAULT_DATA_DIR, dname) as f:
+            return f
+    else:
+        raise FileNotFoundError(dname + " not found in data directory.")
+
+
+def load_data_file(dname):
+    r"""Loads a 2-column csv file
+    """
+    path = locate_data_file(dname)
+    return np.loadtxt(path, delimiter=',').T
+
+
+def cross_section_filename(canonical_reaction_name):
+    s = reaction_name_simplify(canonical_reaction_name)
+    s = reaction_name_to_endf(s)
+    return CROSS_SECTION_PREFIX + s + CROSS_SECTION_FILETYPE
+
+
+def cross_section_data(canonical_reaction_name):
+    filename = cross_section_filename(canonical_reaction_name)
+    return load_data_file(filename)
+
+
+if __name__ == '__main__':
+    from faroes.fusrate.reactionnames import DDHE3_NAME
+
+    print(cross_section_data(DDHE3_NAME).T)
